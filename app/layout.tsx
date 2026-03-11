e5c6b7dd37a7280dea19c91f8a15a75b21c2db0f
ButteWill <wcruttenden001@student.butte.edu>
2026-03-11 13:36:21 -0700

    added gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -152,6 +152,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17709729901"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'AW-17709729901');
+            `,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
